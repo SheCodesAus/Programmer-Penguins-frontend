e243edaca9f4f9bd9d966a945325b8a048052aae
prakadw-dev <prakasitadewi@gmail.com>
2026-04-28 18:53:00 +1000

    added the heroku to API

--- frontend/src/api/applications.js
+++ frontend/src/api/applications.js
@@ -1,4 +1,6 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://jobbuddy-d4878ee60bba.herokuapp.com/";
 
 const APPLICATIONS_PATH = "/api/applications";
 
